b2fd6f7b6e938463cde10e84d83f634294c20df9
Arieh Bibliowicz <vainolo@gmail.com>
2018-03-16 10:44:43 +0800

    Excel sample

---
+++ Random/Excel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = "68.xlsx";
+            FileInfo file = new FileInfo(filePath);
+
+            using(var p = new ExcelPackage())
+            {
+                using (ExcelPackage package = new ExcelPackage(file))
+                {
+                    // get the first worksheet in the workbook
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    int col = 2; //The item description
+                    // output the data in column 2
+                    for (int row = 2; row < 5; row++)
+                        Console.WriteLine("\tCell({0},{1}).Value={2}", row, col, worksheet.Cells[row, col].Value);
+                }
+            }
+        }
+    }
+}
